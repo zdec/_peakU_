8972be912bdd9c4ade66be573f63611424cf9574
zdec <taromaciro@gmail.com>
2022-09-16 15:02:17 -0500

    Delete Post & User

--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -10,7 +10,7 @@
 //Import React
 import React, { useState, useEffect } from 'react';
 //Import React Native
-import { View, Dimensions, StyleSheet, FlatList, TouchableOpacity, ActivityIndicator, SafeAreaView, ToastAndroid } from "react-native";
+import { View, Dimensions, StyleSheet, FlatList, TouchableOpacity, ActivityIndicator, ToastAndroid } from "react-native";
 //Import Components Ui
 import {
 	Layout,
@@ -27,6 +27,8 @@ import {
 import { Ionicons  } from "@expo/vector-icons";
 //Import for Popup Menu
 import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
+//Import AwesomeAlert
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 
 //Export Posts View Structure
@@ -34,8 +36,20 @@ export default function ({ navigation }) {
 
 	const { isDarkmode } = useTheme();
 
+    //Loader Activity
+	const [loading, setLoading] = useState(true);
+
     //State by All Posts
     const [allPosts, setallPosts] = useState([]);
+
+    //Action Delete Progress
+    const [actionProgress, setActionProgress] = useState(false);
+    const [showAlertSession, setShowAlertSession] = useState(false);
+    const [itemSelected, setItemSelected] = useState(null);
+
+    const hideAlert = () => {
+		setShowAlertSession(false)
+	};
 
     //Visible Menus
     let mainMenu = [];
@@ -47,6 +61,36 @@ export default function ({ navigation }) {
 	const showMenu = (item) => {
 		mainMenu[item].show();
 	};
+
+    //Update Post Item
+    const updatePost = (item) => {
+        hideMenu(item.id)
+        navigation.navigate("NewPostsScreen")
+    }
+
+    //Activate Message Alert
+    const confirmDelete = (item) => {
+        setItemSelected(item.id)
+		setShowAlertSession(true)
+	}
+
+    //Deletre Post Item
+    const deletePost = async () => {
+        //Activate Loader
+        setActionProgress(true)
+        let idPost = itemSelected
+        let url = 'https://jsonplaceholder.typicode.com/posts/'+idPost
+        
+        await fetch(url, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(json => {
+            //Set Loader
+			setActionProgress(false)
+            setShowAlertSession(false)
+        })
+    }
 
     //Render Items Post
     const RenderItemPost = ({item}) => (
@@ -61,17 +105,17 @@ export default function ({ navigation }) {
 				<View style={{width: 50, alignItems: "center", marginLeft:3}}>
 					<Menu
                         ref={(menu) => { mainMenu[item.id] = menu }}
-						anchor={<Ionicons name="ellipsis-horizontal-outline" onPress={()=>{showMenu(item.id)}} size={25} color={isDarkmode ? themeColor.success : themeColor.primary400} />}
+						anchor={<Ionicons name="ellipsis-horizontal-outline" onPress={()=>{showMenu(item.id)}} size={25} color={isDarkmode ? themeColor.success : themeColor.dark} />}
 						onRequestClose={()=>{hideMenu(item.id)}}
 						style={{backgroundColor: isDarkmode ? themeColor.dark : themeColor.white}}
 					>
 						
 						<MenuItem onPress={() => {updatePost(item)}}>
-							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Actualizar </Text>
+							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Update </Text>
 						</MenuItem>
 						<MenuDivider />
-						<MenuItem onPress={() => {deletePost(item)}}>
-							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Eliminar </Text>
+						<MenuItem onPress={() => {confirmDelete(item)}}>
+							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Delete </Text>
 						</MenuItem>
 					</Menu>	
 				</View>
@@ -90,7 +134,12 @@ export default function ({ navigation }) {
             }
         })
         .then(response => response.json())
-        .then(json => setallPosts(json))
+        .then(json => {
+            //Set Data
+            setallPosts(json)
+            //Set Loader
+			setLoading(false)
+        })
     }
 	
     //Load Data Posts
@@ -98,6 +147,7 @@ export default function ({ navigation }) {
 		getAllPosts()
 	},[])
 
+    //Return View Posts
 	return (
 	  <Layout>
 		{/* Top Navigator Posts */}
@@ -108,28 +158,78 @@ export default function ({ navigation }) {
 			rightAction={() => navigation.navigate("NewPostsScreen")}
 			leftContent="New Posts"
         	leftTextStyle={{width: 200}}
+            style={styles.topNav}
 		/>
 		{/* Posts View */}
 		<View style={styles.container}>
-			{allPosts.length > 0 ? (
-                <FlatList
-                    data={allPosts}
-                    renderItem={({ item }) => <RenderItemPost item={item} />}
-                    keyExtractor={item => item.id}
-                />
-            ):(
-                <View style={styles.contentNotData}>
-                    <Ionicons
-                        name="clipboard-outline"
-                        size={48}
-                        color={isDarkmode ? themeColor.white200 : "grey"}
+            {loading ? (
+				<ActivityIndicator color="#3366FF" size={40} style={{flex:1, justifyContent:'center'}}/>
+			) : (
+                <>
+                {allPosts.length > 0 ? (
+                    <FlatList
+                        data={allPosts}
+                        renderItem={({ item }) => <RenderItemPost item={item} />}
+                        keyExtractor={item => item.id}
                     />
-                    <Text style={{color:isDarkmode ? themeColor.white200 : "grey"}}>
-                        Don't have Posts. 
-                    </Text>
-                </View>
+                ):(
+                    <View style={styles.contentNotData}>
+                        <Ionicons
+                            name="clipboard-outline"
+                            size={48}
+                            color={isDarkmode ? themeColor.white200 : "grey"}
+                        />
+                        <Text style={{color:isDarkmode ? themeColor.white200 : "grey"}}>
+                            Don't have Posts. 
+                        </Text>
+                    </View>
+                )}
+                </>
             )}
 		</View>
+
+		<AwesomeAlert
+			show={showAlertSession}
+			showProgress={false}
+			closeOnTouchOutside={false}
+			closeOnHardwareBackPress={false}
+			customView={
+				<View>
+					{actionProgress ? (
+						<View>
+							<View>
+								<ActivityIndicator color="#3366FF" size={40}/>
+							</View>
+							<View>
+								<Text style={{color: 'grey'}}>Cargando...</Text>
+							</View>
+						</View>
+					):(
+						<View style={styles.contentContainerAlert}>
+							<View style={styles.contentAlert}>
+								<Text style={styles.titleAlert}>{"Confirm"}</Text>
+								<Text style={styles.messageAlert}>{"Delete post?"}</Text>
+							</View>
+							<View style={styles.actionAlert}>
+								<TouchableOpacity onPress={() => {hideAlert()}}>
+									<View style={[styles.buttonAlert,{backgroundColor: '#D0D0D0'}]}>
+										<Text style={styles.buttonTextAlert}>Cancel</Text>
+									</View>
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {deletePost()}}>
+									<View style={[styles.buttonAlert,{backgroundColor: '#DD6B55'}]}>
+										<Text style={styles.buttonTextAlert}>Accept</Text>
+									</View>
+								</TouchableOpacity>
+							</View>
+						</View>
+					)}
+				</View>
+			}
+			onDismiss={() => {
+
+			}}
+		/>
 	  </Layout>
 	);
 }
@@ -150,6 +250,9 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+    topNav: {
+        marginTop: 0
+    },
 	posts: {
 		flex: 1,
 		justifyContent: "space-between",
@@ -174,7 +277,43 @@ const styles = StyleSheet.create({
 		maxWidth: windowWidth80,
 		overflow: "hidden",
         fontSize: 20,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+		marginBottom: 5
+	},
+	contentContainerAlert: {
+		backgroundColor: 'white',
+	},
+	contentAlert: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10
+	},
+	actionAlert: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		marginTop: 5
+	},
+	titleAlert: {
+		paddingVertical: 5,
+		paddingHorizontal: 15,
+		color: '#626262',
+		fontSize: 18
+	},
+	messageAlert: {
+		paddingTop: 5,
+		color: '#7b7b7b',
+		fontSize: 14
+	},
+	buttonAlert: {
+		paddingHorizontal: 10,
+		paddingVertical: 7,
+		margin: 5,
+		borderRadius: 5
+	},
+	buttonTextAlert: {
+		color: '#fff',
+		fontSize: 13
 	},
 		
 });
--- src/screens/Users.js
+++ src/screens/Users.js
@@ -10,7 +10,7 @@
 //Import React
 import React, { useState, useEffect } from 'react';
 //Import React Native
-import { View, Dimensions, StyleSheet, FlatList, TouchableOpacity, ActivityIndicator, SafeAreaView, ToastAndroid } from "react-native";
+import { View, Dimensions, StyleSheet, FlatList, TouchableOpacity, ActivityIndicator, ToastAndroid } from "react-native";
 //Import Components Ui
 import {
 	Layout,
@@ -27,6 +27,8 @@ import {
 import { Ionicons  } from "@expo/vector-icons";
 //Import for Popup Menu
 import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
+//Import AwesomeAlert
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 
 //Export Users View Structure
@@ -34,8 +36,20 @@ export default function ({ navigation }) {
 
 	const { isDarkmode } = useTheme();
 
+	//Loader Activity
+	const [loading, setLoading] = useState(true);
+
 	//State by All Users
     const [allUsers, setallUsers] = useState([]);
+	
+	//Action Delete Progress
+    const [actionProgress, setActionProgress] = useState(false);
+    const [showAlertSession, setShowAlertSession] = useState(false);
+    const [itemSelected, setItemSelected] = useState(null);
+
+    const hideAlert = () => {
+		setShowAlertSession(false)
+	};
 
     //Visible Menus
     let mainMenu = [];
@@ -48,6 +62,36 @@ export default function ({ navigation }) {
 		mainMenu[item].show();
 	};
 
+	//Update User Item
+    const updateUser = (item) => {
+		hideMenu(item.id)
+        navigation.navigate("NewUsersScreen")
+    }
+
+    //Activate Message Alert
+    const confirmDelete = (item) => {
+        setItemSelected(item.id)
+		setShowAlertSession(true)
+	}
+
+    //Deletre User Item
+    const deleteUser = async () => {
+        //Activate Loader
+        setActionProgress(true)
+        let idUser = itemSelected
+        let url = 'https://jsonplaceholder.typicode.com/users/'+idUser
+        
+        await fetch(url, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(json => {
+            //Set Loader
+			setActionProgress(false)
+            setShowAlertSession(false)
+        })
+    }
+
     //Render Items User
     const RenderItemUser = ({item}) => (
 		<Section>
@@ -55,23 +99,25 @@ export default function ({ navigation }) {
 				<TouchableOpacity style={styles.userrow} >
 					<View style={styles.textrow}>
 						<Text style={styles.name} numberOfLines={1}>@{item.username.toLowerCase()}</Text>
-						<Text style={{color: themeColor.gray300, maxWidth: (Dimensions.get('window').width*0.8)}} numberOfLines={5}>{item.name}</Text>
+						<Text style={styles.userdata} numberOfLines={1}>{item.name}</Text>
+						<Text style={styles.userdata} numberOfLines={1}>{item.email}</Text>
+						<Text style={styles.userdataphone} numberOfLines={1}>{item.phone}</Text>
 					</View>
 				</TouchableOpacity>
 				<View style={{width: 50, alignItems: "center", marginLeft:3}}>
 					<Menu
                         ref={(menu) => { mainMenu[item.id] = menu }}
-						anchor={<Ionicons name="ellipsis-horizontal-outline" onPress={()=>{showMenu(item.id)}} size={25} color={isDarkmode ? themeColor.success : themeColor.primary400} />}
+						anchor={<Ionicons name="ellipsis-horizontal-outline" onPress={()=>{showMenu(item.id)}} size={25} color={isDarkmode ? themeColor.success : themeColor.dark} />}
 						onRequestClose={()=>{hideMenu(item.id)}}
 						style={{backgroundColor: isDarkmode ? themeColor.dark : themeColor.white}}
 					>
 						
-						<MenuItem onPress={() => {updatePost(item)}}>
-							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Actualizar </Text>
+						<MenuItem onPress={() => {updateUser(item)}}>
+							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Update </Text>
 						</MenuItem>
 						<MenuDivider />
-						<MenuItem onPress={() => {deletePost(item)}}>
-							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Eliminar </Text>
+						<MenuItem onPress={() => {confirmDelete(item)}}>
+							<Text style={{color: isDarkmode ? themeColor.white : themeColor.dark}}> Delete </Text>
 						</MenuItem>
 					</Menu>	
 				</View>
@@ -90,7 +136,12 @@ export default function ({ navigation }) {
             }
         })
         .then(response => response.json())
-        .then(json => setallUsers(json))
+        .then(json => {
+			//Load Data
+			setallUsers(json)
+			//Set Loader
+			setLoading(false)
+		})
     }
 	
     //Load Data Users
@@ -98,7 +149,7 @@ export default function ({ navigation }) {
 		getAllUsers()
 	},[])
 	
-
+	//Return View Users
 	return (
 		<Layout>
 		{/* Top Navigator Users */}
@@ -110,27 +161,76 @@ export default function ({ navigation }) {
 			leftContent="New Users"
         	leftTextStyle={{width: 200}}
 		/>
-		{/* Posts View */}
+		{/* Users View */}
 		<View style={styles.container}>
-			{allUsers.length > 0 ? (
-                <FlatList
-                    data={allUsers}
-                    renderItem={({ item }) => <RenderItemUser item={item} />}
-                    keyExtractor={item => item.id}
-                />
-            ):(
-                <View style={styles.contentNotData}>
-                    <Ionicons
-                        name="people-outline"
-                        size={48}
-                        color={isDarkmode ? themeColor.white200 : "grey"}
-                    />
-                    <Text style={{color:isDarkmode ? themeColor.white200 : "grey"}}>
-                        Don't have Users. 
-                    </Text>
-                </View>
-            )}
+			{loading ? (
+				<ActivityIndicator color="#3366FF" size={40} style={{flex:1, justifyContent:'center'}}/>
+			) : (
+				<>
+				{allUsers.length > 0 ? (
+					<FlatList
+						data={allUsers}
+						renderItem={({ item }) => <RenderItemUser item={item} />}
+						keyExtractor={item => item.id}
+					/>
+				):(
+					<View style={styles.contentNotData}>
+						<Ionicons
+							name="people-outline"
+							size={48}
+							color={isDarkmode ? themeColor.white200 : "grey"}
+						/>
+						<Text style={{color:isDarkmode ? themeColor.white200 : "grey"}}>
+							Don't have Users. 
+						</Text>
+					</View>
+				)}
+				</>
+			)}
 		</View>
+
+		<AwesomeAlert
+			show={showAlertSession}
+			showProgress={false}
+			closeOnTouchOutside={false}
+			closeOnHardwareBackPress={false}
+			customView={
+				<View>
+					{actionProgress ? (
+						<View>
+							<View>
+								<ActivityIndicator color="#3366FF" size={40}/>
+							</View>
+							<View>
+								<Text style={{color: 'grey'}}>Cargando...</Text>
+							</View>
+						</View>
+					):(
+						<View style={styles.contentContainerAlert}>
+							<View style={styles.contentAlert}>
+								<Text style={styles.titleAlert}>{"Confirm"}</Text>
+								<Text style={styles.messageAlert}>{"Delete user?"}</Text>
+							</View>
+							<View style={styles.actionAlert}>
+								<TouchableOpacity onPress={() => {hideAlert()}}>
+									<View style={[styles.buttonAlert,{backgroundColor: '#D0D0D0'}]}>
+										<Text style={styles.buttonTextAlert}>Cancel</Text>
+									</View>
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {deleteUser()}}>
+									<View style={[styles.buttonAlert,{backgroundColor: '#DD6B55'}]}>
+										<Text style={styles.buttonTextAlert}>Accept</Text>
+									</View>
+								</TouchableOpacity>
+							</View>
+						</View>
+					)}
+				</View>
+			}
+			onDismiss={() => {
+
+			}}
+		/>
 	  </Layout>
 	);
 }
@@ -175,7 +275,45 @@ const styles = StyleSheet.create({
 		maxWidth: windowWidth80,
 		overflow: "hidden",
         fontSize: 20,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+		marginBottom: 5
+	},
+	userdata: {color: themeColor.gray300, maxWidth: windowWidth80},
+	userdataphone: {color: themeColor.gray300, maxWidth: windowWidth80, textDecorationLine: 'underline'},
+	contentContainerAlert: {
+		backgroundColor: 'white',
+	},
+	contentAlert: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10
+	},
+	actionAlert: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		marginTop: 5
+	},
+	titleAlert: {
+		paddingVertical: 5,
+		paddingHorizontal: 15,
+		color: '#626262',
+		fontSize: 18
+	},
+	messageAlert: {
+		paddingTop: 5,
+		color: '#7b7b7b',
+		fontSize: 14
+	},
+	buttonAlert: {
+		paddingHorizontal: 10,
+		paddingVertical: 7,
+		margin: 5,
+		borderRadius: 5
+	},
+	buttonTextAlert: {
+		color: '#fff',
+		fontSize: 13
 	},
 		
 });
